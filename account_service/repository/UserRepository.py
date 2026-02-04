dddc4abeaa9beaffe23db8738aee3326e0918a88
杨春辉 <13749847+yangchunhui123@user.noreply.gitee.com>
2026-02-04 18:27:20 +0800

    完善common，方便后续开发

--- account_service/repository/UserRepository.py
+++ account_service/repository/UserRepository.py
@@ -1,6 +1,11 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from common.utils.db.AsyncBaseRepository import AsyncBaseRepository
+from account_service.model.User import User
 
 
-class UserRepository(AsyncBaseRepository):
-   
-   pass
+class UserRepository(AsyncBaseRepository[User]):
+    """用户数据访问层"""
+
+    def __init__(self, db: AsyncSession):
+        super().__init__(db, User)
